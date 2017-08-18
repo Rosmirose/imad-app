b53229eeb8bc956183951e3ec3baaf14e452d95f
Rosmi <rosmirehman000@gmail.com>
2017-08-18 23:23:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+app.get('/article-1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+app.get('/article-2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+app.get('/article-1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
